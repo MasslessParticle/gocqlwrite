010f158cc6fad2364b078c869aeb9aa724caae60
Travis Patterson <massless@Traviss-MacBook-Pro-2.local>
2018-10-01 13:20:57 -0600

    setup for write test

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"sync"
+	"sync/atomic"
 	"time"
 
 	"github.com/gocql/gocql"
@@ -11,7 +14,54 @@ import (
 )
 
 func main() {
-	//Create the table
+	var success uint64
+	var failed uint64
+	defer func() {
+		fmt.Printf("Success: %d\n", atomic.LoadUint64(&success))
+		fmt.Printf("Failed: %d\n", atomic.LoadUint64(&failed))
+	}()
+
+	delay, err := time.ParseDuration(os.Getenv("delay"))
+	if err != nil {
+		panic(err)
+	}
+	for i := 0; i < 1; i++ {
+		go func() {
+			session := connect()
+			defer session.Close()
+
+			guid := guid()
+			t := time.NewTicker(delay)
+			for range t.C {
+				err := insert(guid, session)
+				if err != nil {
+					atomic.AddUint64(&failed, 1)
+					log.Println(err)
+					continue
+				}
+
+				atomic.AddUint64(&success, 1)
+			}
+		}()
+	}
+
+	wait()
+}
+
+func wait() {
+	var end_waiter sync.WaitGroup
+	end_waiter.Add(1)
+	var signal_channel chan os.Signal
+	signal_channel = make(chan os.Signal, 1)
+	signal.Notify(signal_channel, os.Interrupt)
+	go func() {
+		<-signal_channel
+		end_waiter.Done()
+	}()
+	end_waiter.Wait()
+}
+
+func connect() *gocql.Session {
 	cluster := gocql.NewCluster()
 	cluster.Authenticator = gocql.PasswordAuthenticator{Username: os.Getenv("USER"), Password: os.Getenv("PASSWORD")}
 	cluster.Keyspace = "gocqlwrite"
@@ -21,40 +71,30 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	defer session.Close()
 
 	fmt.Println("Create Table")
 	if err := session.Query(`
 		CREATE TABLE IF NOT EXISTS logs (
 		   source_id varchar,
-		   timestamp_minute varchar,
 		   ts timeuuid,
 		   log varchar,
-		PRIMARY KEY (source_id, timestamp_minute));`).Exec(); err != nil {
+		PRIMARY KEY (source_id, ts);`).Exec(); err != nil {
 		log.Fatal(err)
 	}
-
-	sourceID := sourceID()
-	fmt.Println("***********************************")
-	fmt.Println(sourceID)
-	fmt.Println("***********************************")
-
-	now := time.Now()
-
-	fmt.Println("Write Data")
-	if err := session.Query(`
-				INSERT into logs (source_id, timestamp_minute, ts, log) VALUES(?, ?, ?, ?)`,
-		sourceID,
-		fmt.Sprintf("%s", now.Truncate(time.Minute).UnixNano()),
-		now,
-		"sample log message "+sourceID,
-	).Exec(); err != nil {
-		log.Fatal(err)
-	}
-
+	return session
 }
 
-func sourceID() string {
+func insert(guid string, s *gocql.Session) error {
+	now := time.Now()
+	return s.Query(`
+				INSERT into logs (source_id, ts, log) VALUES(?, ?, ?)`,
+		guid,
+		now,
+		"sample log message "+guid,
+	).Exec()
+}
+
+func guid() string {
 	uuid, _ := uuid.NewV4()
 	return uuid.String()
 }
