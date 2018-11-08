7901534960d21ff8e0d43a0f637a16b976b31c39
John Foley <jfoley@pivotal.io>
2018-11-07 19:08:16 -0700

    emit static amount of metrics from cf apps
    Signed-off-by: John Cornish <jcornish@pivotal.io>

--- main.go
+++ main.go
@@ -3,105 +3,71 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 	"os"
 	"sync/atomic"
 	"time"
 
+	"github.com/brianvoe/gofakeit"
 	"github.com/gocql/gocql"
 	uuid "github.com/nu7hatch/gouuid"
 )
 
+var (
+	success uint64
+	failed  uint64
+)
+
 func main() {
-	http.HandleFunc("/run_test", func(w http.ResponseWriter, r *http.Request) {
-		go runTest()
-		w.Write([]byte("Test Started!"))
-	})
-	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-}
-
-func runTest() {
-	var success uint64
-	var failed uint64
-	defer func() {
-		fmt.Printf("Success: %d\n", atomic.LoadUint64(&success))
-		fmt.Printf("Failed: %d\n", atomic.LoadUint64(&failed))
-	}()
-
-	delay, err := time.ParseDuration(os.Getenv("delay"))
-	if err != nil {
-		panic(err)
-	}
-
 	fmt.Println("Create Table")
 	c := setupConnction()
 
 	fmt.Println("Create Sessions")
-	ps := 125
-	sessions := make(chan *gocql.Session, 500)
-	createSessions(c, sessions, ps)
+	session, err := c.CreateSession()
+	if err != nil {
+		panic(err)
+	}
 
 	fmt.Println("Write")
-	doneChan := make(chan struct{}, 1)
-	for i := 0; i < ps; i++ {
-		guid := guid()
-		fmt.Println(guid)
+	guid := guid()
+	fmt.Println(guid)
 
-		go func() {
-			session := <-sessions
-			t := time.NewTicker(delay)
-			for {
-				select {
-				case <-t.C:
-					logMessage := sampleLogMessage()
+	go serveStats(guid)
 
-					batch := session.NewBatch(gocql.LoggedBatch)
-					for i := 0; i < 20000/ps; i++ {
-						batch.Query(`INSERT into logs (source_id, ts_nanos, log) VALUES(?, ?, ?)`, guid, time.Now().UnixNano(), logMessage)
-					}
+	t := time.NewTicker(1 * time.Millisecond)
+	for range t.C {
+		batch := session.NewBatch(gocql.LoggedBatch)
+		for i := 0; i < 5; i++ {
+			now := time.Now()
+			nowMin := now.Truncate(time.Hour)
 
-					err = session.ExecuteBatch(batch)
-					if err != nil {
-						atomic.AddUint64(&failed, 20000/uint64(ps))
-						log.Println(err)
-					}
-
-					atomic.AddUint64(&success, 20000/uint64(ps))
-				case <-doneChan:
-					return
-				}
-			}
-		}()
-	}
-
-	testDuration := time.NewTimer(time.Minute)
-	<-testDuration.C
-	close(doneChan)
-}
-
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-func sampleLogMessage() string {
-	b := make([]byte, 200)
-	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
-	}
-	return string(b)
-}
-
-func createSessions(c *gocql.ClusterConfig, sessions chan *gocql.Session, poolSize int) {
-	for i := 0; i < poolSize; i++ {
-
-		session, err := c.CreateSession()
-		if err != nil {
-			panic(err)
+			batch.Query(`INSERT into logs (source_id, ts_hour, ts, log) VALUES(?, ?, ?, ?)`, guid, nowMin, now.UnixNano(), gofakeit.HackerPhrase())
 		}
-		fmt.Print(i)
-		sessions <- session
 
+		err = session.ExecuteBatch(batch)
+		if err != nil {
+			atomic.AddUint64(&failed, 5)
+			log.Println(err)
+			continue
+		}
+
+		atomic.AddUint64(&success, 5)
 	}
-	fmt.Println()
+}
+
+func serveStats(appId string) {
+	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		stats := fmt.Sprintf(
+			`{"id": "%s", "success": "%d", "failed": "%d"}`,
+			appId,
+			atomic.LoadUint64(&success),
+			atomic.LoadUint64(&failed),
+		)
+
+		w.Write([]byte(stats))
+	})
+
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
 }
 
 func setupConnction() *gocql.ClusterConfig {
@@ -117,7 +83,13 @@ func setupConnction() *gocql.ClusterConfig {
 		panic(err)
 	}
 
-	if err := session.Query(`CREATE TABLE IF NOT EXISTS logs (source_id varchar, ts_nanos bigint, log varchar, PRIMARY KEY (source_id, ts_nanos));`).Exec(); err != nil {
+	if err := session.Query(`
+		CREATE TABLE IF NOT EXISTS logs (
+		   source_id varchar,
+		   ts_hour varchar,
+		   ts bigint,
+		   log varchar,
+		PRIMARY KEY ((source_id, ts_min), ts));`).Exec(); err != nil {
 		log.Fatal(err)
 	}
 	session.Close()
